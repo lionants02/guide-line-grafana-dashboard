66982b6244c4ca86eeff8c877612de20f484947b
Max Thanachai Thongkum <singmax161@gmail.com>
2023-01-13 15:00:39 +0700

    add grafana deploy

---
+++ grafana/deploy.sh
@@ -0,0 +1,25 @@
+set -a
+source env.env
+
+echo "S_DATA=$S_DATA"
+echo "SERVICE_NAME=$SERVICE_NAME"
+
+echo "Create folder $S_DATA/datasources"
+mkdir -p $S_DATA/datasources
+echo "Create folder $S_DATA/lib_grafana"
+mkdir -p $S_DATA/lib_grafana
+
+mkdir -p $S_DATA/dashboard
+
+touch $S_DATA/dashboard/dashboard.json
+
+
+#cp grafana.ini $S_DATA/grafana.ini
+
+ln -s $(pwd)/grafana.ini $S_DATA/grafana.ini
+
+echo "Change mod foler $S_DATA/grafana"
+chmod -R 777 $S_DATA
+
+echo "Deploy stack"
+docker stack deploy -c ./stack.yml $SERVICE_NAME
